c7782447b9fdaa05ff6ab80d659674ca6efa5a5c
Adarsh kumar <adarshku123456789@gmail.com>
2025-05-18 16:42:45 +0530

    Themes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,31 +1,33 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import './globals.css'
+import { Poppins, Josefin_Sans } from 'next/font/google'
+import { ThemeProvider } from './utils/theme-provider'
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+const poppins = Poppins({
+  variable: '--font-Poppins',
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900']
+})
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const josefin = Josefin_Sans({
+  variable: '--font-Josefin',
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700']
+})
 
-
-
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout ({
+  children
+}: {
+  children: React.ReactNode
+}) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${poppins.variable} ${josefin.variable} !bg-white dark:bg-gradient-to-b dark:from-gray-900 dark:to-black duration-300`}
       >
-        {children}
+        <ThemeProvider attribute='class' defaultTheme='system' enableSystem>
+          {children}
+        </ThemeProvider>
       </body>
     </html>
-  );
+  )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,8 +6,9 @@ const config: Config = {
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",,
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: ["class"],
   theme: {
     extend: {
       fontFamily: {
