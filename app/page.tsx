5a5329308dcf41614fe99fcafdb9418562a95a71
Adarsh kumar <adarshku123456789@gmail.com>
2025-05-18 18:31:14 +0530

    NaveItems

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,29 @@
 'use client'
 
-import React, { FC } from 'react'
+import React, { FC, useState } from 'react'
+
 import Heading from './utils/Heading'
+import Headers from './components/Headers'
 
 interface Props {}
 
 const Page: FC<Props> = () => {
+  const [open, setOpen] = useState(false)
+  const [activeItem, setActiveItem] = useState(0)
+
   return (
     <div>
-    <Heading
-      title="Elearning"
-      description="This is a description of my page."
-      keywords="programming, react, nextjs, typescript, web development,ml, ai"  
+      <Heading
+        title='Elearning'
+        description='This is a description of my page.'
+        keywords='programming, react, nextjs, typescript, web development,ml, ai'
       />
       
+      <Headers
+        open={open}
+        setOpen={setOpen}
+        activeItem={activeItem}
+      />
     </div>
   )
 }
--- app/utils/Heading.tsx
+++ app/utils/Heading.tsx
@@ -10,7 +10,7 @@ const Heading: FC<HeadProps> = ({ title, description, keywords }) => {
   return (
     <head>
       <title>{title}</title>
-      <meta name="viewport" content="width = device-width, initial-scale-1"/>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
       <meta name="description" content={description} />
       <meta name="keywords" content={keywords} />
     </head>
